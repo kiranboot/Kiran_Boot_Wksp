49b18197a35d125d0d93d3769cab84743f3834a3
kiran <kiranreddyamk@gmail.com>
2017-04-23 23:23:32 -0400

    Initial Commit

---
+++ video8/src/main/java/com/kiran/sb/KiranApp.java
@@ -0,0 +1,13 @@
+package com.kiran.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiranApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiranApp.class, args);
+	}
+
+}
