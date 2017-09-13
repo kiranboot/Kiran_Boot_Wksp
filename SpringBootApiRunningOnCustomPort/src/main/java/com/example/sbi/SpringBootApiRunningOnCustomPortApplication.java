6f4157ff426211b3f132a37c36c9d414487433b1
Kiran <kiranreddyamk@gmail.com>
2017-09-12 23:52:05 -0400

    Initial Commit

---
+++ SpringBootApiRunningOnCustomPort/src/main/java/com/example/sbi/SpringBootApiRunningOnCustomPortApplication.java
@@ -0,0 +1,12 @@
+package com.example.sbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiRunningOnCustomPortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiRunningOnCustomPortApplication.class, args);
+	}
+}
