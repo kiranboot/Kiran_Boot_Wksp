d53ac3caeea9eb016847522515db5526aeb0119e
Kiran <kiranreddyamk@gmail.com>
2017-09-25 17:55:02 -0400

    Initial Commit

---
+++ BuildAndRunDockerImageUsingMavenPlugin/src/main/java/com/example/demo/BuildAndRunDockerImageUsingMavenPluginApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildAndRunDockerImageUsingMavenPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildAndRunDockerImageUsingMavenPluginApplication.class, args);
+	}
+}
